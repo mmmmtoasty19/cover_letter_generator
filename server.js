1a2591e005f021c46e1741278f8c3be5bdf38638
Kyle Belanger <Kyleb44@hotmail.com>
2025-02-13 15:56:29 -0500

    update server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;  //TODO DO NOT HARDCODE THIS
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+
+// Routes
+// const generateRoute = require('./routes/generate');
+// app.use('/generate', generateRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
